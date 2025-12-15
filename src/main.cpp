8f9f8e1c3f2e65fbd9ad7153d1d9077c5ed7abc8
Marcelo <marcelobro.pera@gmail.com>
2025-12-15 16:23:45 +0100

    Implemented decoder

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include "io/byte_reader.hpp"
+#include "model/raw_frame.hpp"
+#include "model/frame_parser.hpp"
 #include <cstddef>   // std::byte
 #include <cstdint>   // uint8_t
 #include <utility>   // std::to_integer
@@ -20,11 +22,26 @@ int main() {
     
   printf("Read %zu bytes:\n", bytes_read);
     for (std::size_t i{0}; i < bytes_read; ++i) {
-        auto b = std::to_integer<std::uint8_t>(buffer[i]);
+        std::uint8_t b = byte_value(buffer[i]);
         printf("%02x ", static_cast<unsigned int>(b));
     }
-    printf("\n");
+        for (std::size_t i{0}; i < bytes_read; ++i) {
+        std::uint8_t b = byte_value(buffer[i]);
+        if (b==0x5a && byte_value(buffer[i+1])==0xa5){
+          printf("\n Sync found at offset %zu", i);
+        }
+    }
+
   // parse frames
+  
+  RawFrame decoded_frame = decode_frame(buffer, 0);
+  printf("\n Timestamp %d", decoded_frame.timestamp);
+  printf("\n Speed %d", decoded_frame.speed);
+  printf("\n Voltage %d", decoded_frame.dc_voltage);
+  printf("\n Temperature %d", decoded_frame.temperature);
+  printf("\n Status %d \n", decoded_frame.status_flags);
+  
+
   // for now, just a placeholder—we'll print a fake output summary
   // printf("________________________________________________________\n");
   // printf("Frames received: 1234\n");
@@ -36,6 +53,6 @@ int main() {
   // printf("Warnings: \n \t Overvoltage events: 2 \n \t Overtemperature events:
   // "
   //        "1 \n\n");
-  // printf("________________________________________________________\n");
+  printf("________________________________________________________\n");
   printf("Telemetry decoding completed");
 }
